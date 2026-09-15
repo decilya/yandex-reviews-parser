0dc5d999cbc281f4306d775ff646eddb67cd82e0
decilya <decilya@gmail.com>
2026-09-15 22:55:27 +0300

    fix: восстановить main.js и api.js, добавить package-lock

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,31 +1,4 @@
-import axios from 'axios';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || '',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest',
-    },
-});
-
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
-
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            localStorage.removeItem('token');
-            window.location.reload();
-        }
-        return Promise.reject(error);
-    }
-);
-
-export default api;
+createApp(App).mount('#app');
